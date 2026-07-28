baa52a4c173efea3fbe1955546fc01909e704fd2
metaverse-test <hglobe624@gmail.com>
2026-07-28 13:21:48 +0000

    controls.js

---
+++ js/controls.js
@@ -0,0 +1,12 @@
+// Ce fichier peut être utilisé pour des contrôles supplémentaires
+// ou pour étendre les contrôles existants
+
+export class ControlsManager {
+    constructor() {
+        this.controls = null;
+    }
+    
+    init(camera, renderer) {
+        // Configuration supplémentaire des contrôles
+    }
+}
